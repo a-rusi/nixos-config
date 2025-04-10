eed556ce21a86383c159b4f305dabe074c0559fe
Alejandro Rusi <alejandro.rusi@gmail.com>
2025-04-10 10:46:39 -0300

    update home manager config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,20 +1,12 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{ config, pkgs, ... }: {
   home.username = "arusi";
   home.homeDirectory = "/home/arusi";
 
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-    slack # For getting pinged at work
     duckdb # For command-line SQL
     htop # For checking current computer usage
     fd # Replacement for find command
@@ -30,17 +22,22 @@
     tldr # Simplified man pages
     tree # For printing directory listing
     watch # For observing changes on long running processes
-    skim # Like fzf (runs with the sk command)
+    skim # Like fzf (runs with the sk command
     xclip # For using the clipboard in X11 systems
     vscode # Visual Studio Code
     pass # The pass command, for locally storing passwords
     neofetch # For bragging in the internet
-    obsidian # For daily writing
-
-    # For generating keys
-    gnupg
-    pinentry
-    
+    starship # Pretty prompts
+    imagemagick # Image transformation, transform file format to another
+    black # Python formatter
+    dart-sass # For the premium edition of Hugo
+    pipx # Install Python Packages in isolated envs
+    httpie # For easily hitting http endpoint, usage: http GET endpoint
+    pgcli # For connecting to Postgres, usage: pgcli -h localhost -U postgres
+    fzf # Fuzzy finder for searching files, usage: find . -name "*.sql" | fzf
+    lnav # for navigating log files, usage: lnav /var/log/syslog
+    visidata # for visualizing sheets, usage: vd hw_200.csv
+    delta # better git diff, usage: git diff | delta
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -50,28 +47,8 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/arusi/etc/profile.d/hm-session-vars.sh
-  #
   programs.helix = {
     enable = true;
     settings = {
@@ -82,17 +59,15 @@
         select = "underline";
       };
     };
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-      }
-    ];
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }];
     themes = {
       autumn_night_transparent = {
         "inherits" = "autumn_night";
-        "ui.background" = {};
+        "ui.background" = { };
       };
     };
   };
@@ -125,48 +100,18 @@
   # and records additional context for your commands.
   # Additionally, it provides optional and fully encrypted
   # synchronisation of your history between machines, via an Atuin server.
-  programs.atuin = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableNushellIntegration = true;
-  };
-  programs.zellij = {
-    enable = true;
-  };
+  programs.zellij = { enable = true; };
 
-  programs.nushell = {
-    enable = true;
-    configFile.source = ./config.nu;
-  };
+  programs.rofi = { enable = true; };
 
-  programs.rofi = {
-    enable = true;
-  };
-
-  programs.alacritty = {
-    enable = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "a-rusi";
-  };
-
-  programs.gh = {
-    enable = true;
-    gitCredentialHelper.enable = true;
-  };
   home.sessionVariables = {
     EDITOR = "hx";
     BROWSER = "firefox";
-    TERMINAL = "alacritty";
+    TERMINAL = "nu";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  xdg.configFile = {
-    "alacritty/alacritty.yml".source = ./alacritty.yml;
-  };
+  xdg.configFile = { "alacritty/alacritty.yml".source = ./alacritty.yml; };
 }
